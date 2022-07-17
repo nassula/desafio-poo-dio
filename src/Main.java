931a96392faf10403338650a42423caa33629e03
Diego Nassula <diegonassula@gmail.com>
2022-07-17 15:23:52 -0300

    Subindo ultimas alterações feitas no projeto

--- src/Main.java
+++ src/Main.java
@@ -2,65 +2,86 @@ import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
-
+import java.util.Random;
 import java.time.LocalDate;
 
+//Classe onde irá iniciar a execução do programa
 public class Main {
+
+    //método principal de execução
     public static void main(String[] args) {
 
-        //Just testing
+        String[] titulos = {"Formação Java","Formação JS","Curso React","Formação Laravel"};
+        String[] descricoes = {
+                "Formação Java completa do junior ao senior, tudo o que você precisa aprender para ser um dev completo",
+                "Formação JavaScript para Backend utilizando Node.JS, aprenda a liguagem mais utilizada hoje na internet!",
+                "Aprenda tudo que o que o React tem a oferecer para FrontEnd.",
+                "Formação Laravel completa do junior ao senior, tudo o que você precisa aprender para ser um dev completo"};
+        int[] cargaHorarias = {25,25,8,25};
 
-        Curso curso1 = new Curso();
-        curso1.setTitulo("curso java");
-        curso1.setDescricao("descrição curso java");
-        curso1.setCargaHoraria(8);
+        Curso[] cursos = new Curso[4];
 
-        Curso curso2 = new Curso();
-        curso2.setTitulo("curso js");
-        curso2.setDescricao("descrição curso js");
-        curso2.setCargaHoraria(4);
+        for(int i = 0 ; i < 4 ; i++){
+            cursos[i] = new Curso();
+            cursos[i].setTitulo(titulos[i]);
+            cursos[i].setDescricao(descricoes[i]);
+            cursos[i].setCargaHoraria(cargaHorarias[i]);
+        }
 
-        Mentoria mentoria = new Mentoria();
-        mentoria.setTitulo("mentoria de java");
-        mentoria.setDescricao("descrição mentoria java");
-        mentoria.setData(LocalDate.now());
 
-        /*System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);*/
+        Mentoria mentoriaJava = new Mentoria();
+        mentoriaJava.setTitulo("Mentoria Java");
+        mentoriaJava.setDescricao("Receba toda ajuda para começar a desenvolver utilizando Java");
+        mentoriaJava.setData(LocalDate.now());
 
-        Bootcamp bootcamp = new Bootcamp();
-        bootcamp.setNome("Bootcamp Java Developer");
-        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
-        bootcamp.getConteudos().add(curso1);
-        bootcamp.getConteudos().add(curso2);
-        bootcamp.getConteudos().add(mentoria);
 
-        Dev devCamila = new Dev();
-        devCamila.setNome("Camila");
-        devCamila.inscreverBootcamp(bootcamp);
-        System.out.println("Conteúdos Inscritos Camila:" + devCamila.getConteudosInscritos());
-        devCamila.progredir();
-        devCamila.progredir();
-        System.out.println("-");
-        System.out.println("Conteúdos Inscritos Camila:" + devCamila.getConteudosInscritos());
-        System.out.println("Conteúdos Concluídos Camila:" + devCamila.getConteudosConcluidos());
-        System.out.println("XP:" + devCamila.calcularTotalXp());
+        Bootcamp bootcampJava = new Bootcamp();
+        bootcampJava.setNome("Bootcamp Java Developer");
+        bootcampJava.setDescricao("Uma semana intensa de programação em Java para você mergulhar nessa tecnologia que move o mundo digital");
+        bootcampJava.getConteudos().add(cursos[0]);
+        bootcampJava.getConteudos().add(cursos[1]);
+        bootcampJava.getConteudos().add(mentoriaJava);
 
-        System.out.println("-------");
+        Mentoria mentoriaLaravel = new Mentoria();
+        mentoriaLaravel.setTitulo("Mentoria Laravel");
+        mentoriaLaravel.setDescricao("Receba toda ajuda para começar a desenvolver utilizando Java");
+        mentoriaLaravel.setData(LocalDate.now());
 
-        Dev devJoao = new Dev();
-        devJoao.setNome("Joao");
-        devJoao.inscreverBootcamp(bootcamp);
-        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
-        devJoao.progredir();
-        devJoao.progredir();
-        devJoao.progredir();
-        System.out.println("-");
-        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
-        System.out.println("Conteúdos Concluidos João:" + devJoao.getConteudosConcluidos());
-        System.out.println("XP:" + devJoao.calcularTotalXp());
 
+        Bootcamp bootcampLaravel = new Bootcamp();
+        bootcampLaravel.setNome("Bootcamp Laravel Developer");
+        bootcampLaravel.setDescricao("Uma semana intensa de programação em PHP para você mergulhar nessa tecnologia que domina o mundo backend");
+        bootcampLaravel.getConteudos().add(cursos[2]);
+        bootcampLaravel.getConteudos().add(cursos[3]);
+        bootcampLaravel.getConteudos().add(mentoriaLaravel);
+
+        Dev[] devs = new Dev[3];
+        String[] nomes = {"Diego","Camila","Roberto"};
+
+        Random random = new Random();
+        int numeroMagico = 0;
+        for(int i = 0 ; i < 3 ; i++){
+            devs[i] = new Dev();
+            if(numeroMagico <= 1 ){
+                devs[i].setNome(nomes[i]);
+                devs[i].inscreverBootcamp(bootcampJava);
+            } else {
+                devs[i].setNome(nomes[i]);
+                devs[i].inscreverBootcamp(bootcampLaravel);
+            }
+            for(int j = 0; j < random.nextInt(10);j++){
+                devs[i].progredir();
+            }
+            numeroMagico++;
+        }
+        System.out.println("Saída:");
+        System.out.println("|-------------------------------------------------------------------|");
+        for(int i = 0 ; i < 3 ; i++){
+            System.out.println("Conteúdos Inscritos " + devs[i].getNome() + " :" + devs[i].getConteudosInscritos());
+            System.out.println("Conteúdos Concluidos " + devs[i].getNome() + " :" + devs[i].getConteudosConcluidos());
+            System.out.println("Total de XP:" + devs[i].calcularTotalXp());
+            System.out.println("|-------------------------------------------------------------------|");
+        }
     }
 
 }
